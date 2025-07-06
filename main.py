b154fc17a4798209c16c498c9e5a3c429f9a4b0a
Your Name <your.email@example.com>
2025-07-06 12:23:45 +0530

    added data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from winequality.logging import logger
+from winequality.pipeline.data_ingestion_1 import DataIngestionTrainingPipeline
 
-logger.info("This is the custom log")
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- template.py
+++ template.py
@@ -9,15 +9,15 @@ project_name = "winequality"
 
 list_of_files = [
     rf"src\{project_name}\__init__.py",
-    rf"src{project_name}\components\__init.py",
+    rf"src{project_name}\components\__init__.py",
     rf"src{project_name}\utils\__init__.py",
     rf"src{project_name}\utils\common.py",
-    rf"src{project_name}\config\__init.py",
+    rf"src{project_name}\config\__init__.py",
     rf"src{project_name}\config\configuration.py",
     rf"src{project_name}\pipeline\__init__.py",
     rf"src{project_name}\entity\__init__.py",
     rf"src{project_name}\entity\config_entity.py",
-    rf"src{project_name}\constants\__init.py",
+    rf"src{project_name}\constants\__init__.py",
     "config/config.yaml",
     "params.yaml",
     "schema.yaml",
